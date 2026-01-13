b44656f8f2b9733d2a52a4594a8035f3c6866025
Imane Ramadane <ramadaneimane@gmail.com>
2026-01-13 18:00:53 +0100

    bank

---
+++ Person.java
@@ -0,0 +1,45 @@
+public class Person {
+    private int id;
+    private String nom;
+    private String email;
+    public Person(int id,String nom,String email ){
+        this.id = id ;
+        this.nom = nom;
+        this.email = email;
+
+    }
+    public int getId(){
+        return this.id;
+    }
+    public String getNom(){
+        return this.nom;
+
+    }
+    public String email(){
+         return this.email;
+    }
+
+    public void setId(int id){
+        this.id = id ;
+
+
+
+    }
+    public void setNom(String nom){
+        this.nom = nom;
+
+
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
